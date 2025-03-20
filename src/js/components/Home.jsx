5adfa35623e8a9d389fbd341cde19c8c02502cf7
Jonas <jonas.backline@gmail.com>
2025-03-20 15:00:15 +0000

    2nd commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -8,7 +8,11 @@ import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 //create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState ("")
-	const [todos, setTodos] = useState("")
+	const [todos, setTodos] = useState([])
+
+// Add into array -> concat
+// Delete from array -> filter
+// Update -> map
 
 	return (
 		<div className="container">
@@ -20,25 +24,31 @@ const Home = () => {
 					type="text" 
 					onChange={(e) => setInputValue(e.target.value)}
 					value={inputValue}
-					placeholder="Qué tienes por hacer?" 
+					onKeyPress={(e) => {
+						if(e.key === "Enter") { 
+							setTodos (todos.concat(inputValue))
+							setInputValue("")
+							}
+						}}
+						placeholder="Qué tienes por hacer?" 
 					/> 
 				</li>
-				<li>
-					make the bed
-					<FontAwesomeIcon icon={faTrashAlt} />
+				{todos.map((item, index) => (
+					<li>
+						{item}{""}
+					<FontAwesomeIcon icon={faTrashAlt} 
+						onClick={() => 
+							setTodos(
+								todos.filter(
+									(t, currentIndex) => 
+										index != currentIndex
+								)
+							)
+						}
+					/>
 				</li>
-				<li>
-					Walk the dog
-					<FontAwesomeIcon icon={faTrashAlt} />
-				</li>
-				<li>
-					Pay taxes
-					<FontAwesomeIcon icon={faTrashAlt} />
-				</li>
-				<li>
-					Go on vacation
-					<FontAwesomeIcon icon={faTrashAlt} />
-				</li>
+				))}
+				
 			</ul>
 			<div>Tasks Counter?</div>
 		</div>
